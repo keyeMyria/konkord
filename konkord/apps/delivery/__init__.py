1268fd036248b06027f1fe88fda8ae3e8180c7e3
s_gnitiy <sadjuk17@gmail.com>
2017-02-15 15:21:24 +0200

    removed prints

--- konkord/apps/delivery/__init__.py
+++ konkord/apps/delivery/__init__.py
@@ -1,5 +1,4 @@
 from adminconfig import register
 from .configurer import DeliveryConfig
 default_app_config = 'delivery.apps.DeliveryConfig'
-print(321)
 register(DeliveryConfig)
--- konkord/core/__init__.py
+++ konkord/core/__init__.py
@@ -2,5 +2,4 @@ from adminconfig import register
 from .configurer import CoreConfig
 
 register(CoreConfig)
-print('core')
 default_app_config = 'core.apps.CoreConfig'
