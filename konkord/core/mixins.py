19093bb4c02337a7bd1a7ffc90f032369810d0d9
s_gnitiy <sadjuk17@gmail.com>
2017-02-22 12:51:25 +0200

    added page_type to page context

--- konkord/core/mixins.py
+++ konkord/core/mixins.py
@@ -4,9 +4,13 @@ from django.utils.translation import ugettext_lazy as _
 
 class MetaMixin(object):
     breadcrumbs = [(_('Main page'), '/')]
+    page_type = None
 
     def get_breadcrumbs(self):
         return []
+
+    def get_page_type(self):
+        return self.__class__.__name__.lower().replace('view', '')
 
     def get_context_data(self, **kwargs):
         context = super(MetaMixin, self).get_context_data(**kwargs)
@@ -15,4 +19,5 @@ class MetaMixin(object):
         if additional_breadcrumbs:
             breadcrumbs.extend(additional_breadcrumbs)
         context['breadcrumbs'] = breadcrumbs
+        context['page_type'] = self.get_page_type()
         return context
