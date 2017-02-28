5bf4211e3c12d118f4443f12e8eb4091f0ed0fb6
s_gnitiy <sadjuk17@gmail.com>
2017-02-28 11:01:00 +0200

    removed null from update_times field

--- konkord/apps/price_navigator/models.py
+++ konkord/apps/price_navigator/models.py
@@ -7,41 +7,40 @@ from django.contrib.postgres.fields import JSONField
 
 class PriceNavigator(models.Model):
     CODINGS = (
-        (0, _(u'utf-8')),
-        (5, _(u'windows-1251')),
+        (0, _('utf-8')),
+        (5, _('windows-1251')),
     )
 
-    active = models.BooleanField(_(u'Active'), default=False)
-    name = models.CharField(_(u'Name'), max_length=100)
-    file_name = models.CharField(_(u'File name'), max_length=100)
+    active = models.BooleanField(_('Active'), default=False)
+    name = models.CharField(_('Name'), max_length=100)
+    file_name = models.CharField(_('File name'), max_length=100)
     update_times = models.CharField(
-        _(u'Update rate'),
+        _('Update rate'),
         max_length=300,
-        help_text=_(u'08:00, 20:00'),
-        null=True,
+        help_text=_('08:00, 20:00'),
     )
-    coding = models.IntegerField(_(u'Coding'), default=0, choices=CODINGS)
+    coding = models.IntegerField(_('Coding'), default=0, choices=CODINGS)
     product_statuses = models.ManyToManyField(
         ProductStatus,
-        verbose_name=_(u'Statuses')
+        verbose_name=_('Statuses')
     )
-    pack_to_zip = models.BooleanField(_(u'Pack to zip'), default=False)
-    template = models.TextField(_(u'Template'))
+    pack_to_zip = models.BooleanField(_('Pack to zip'), default=False)
+    template = models.TextField(_('Template'))
     replacement = JSONField(
-        verbose_name=_(u'Symbol replacement'),
+        verbose_name=_('Symbol replacement'),
         blank=True,
         null=True,
         default=''
     )
     order = models.IntegerField(default=10)
     last_generation = models.DateTimeField(
-        _(u"Last generation"),
+        _("Last generation"),
         auto_now=True,
     )
 
     class Meta:
-        verbose_name = _(u'Price navigator')
-        verbose_name_plural = _(u'Price navigators')
+        verbose_name = _('Price navigator')
+        verbose_name_plural = _('Price navigators')
 
     def __str__(self):
         return self.name
